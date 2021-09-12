2c624a16a80b8c40157ca7f9b180b4c70d5f299f
Tribulations <kung_jocke@hotmail.com>
2021-09-12 19:20:42 +0200

    show the gameover screen on collission

--- app/src/main/java/com/example/flappybird/MainActivity.java
+++ app/src/main/java/com/example/flappybird/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.flappybird;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
