264362a973f0200e607d8461735c6f174d30a4ee
Tribulations <kung_jocke@hotmail.com>
2021-09-12 11:49:18 +0200

    gameplay class creation

--- app/src/main/java/com/example/flappybird/GamePlay.java
+++ app/src/main/java/com/example/flappybird/GamePlay.java
@@ -9,26 +9,45 @@ import androidx.annotation.NonNull;
 public class GamePlay extends SurfaceView implements SurfaceHolder.Callback
 {
 
+    private MainThread mainThread;
+
     public GamePlay(Context context)
     {
         super(context);
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+        mainThread = new MainThread(surfaceHolder);
     }
 
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder)
     {
+        mainThread.setIsRunning(true);
+        mainThread.start();
 
     }
 
     @Override
-    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height)
-    {
-
-    }
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {}
 
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder)
     {
+        boolean retry = true;
 
+        while(retry)
+        {
+            try
+            {
+                mainThread.setIsRunning(false);
+                mainThread.join();
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+
+            retry = false;
+        }
     }
 }
---
+++ app/src/main/java/com/example/flappybird/MainThread.java
@@ -0,0 +1,82 @@
+package com.example.flappybird;
+
+import android.graphics.Canvas;
+import android.os.SystemClock;
+import android.view.SurfaceHolder;
+
+public class MainThread extends Thread
+{
+    SurfaceHolder surfaceHolder;
+    long timeSpent;
+    long kickOffTime;
+    long WAIT = 31; // the time it takes for the frame to refresh in ms
+    boolean running;
+    private static Canvas canvas;
+    public MainThread(SurfaceHolder surfaceHolder)
+    {
+        this.surfaceHolder = surfaceHolder;
+        running = true;
+    }
+
+    @Override
+    public void run()
+    {
+        while(running)
+        {
+            kickOffTime = SystemClock.uptimeMillis();
+            canvas = null;
+            try
+            {
+                canvas = surfaceHolder.lockCanvas();
+
+                synchronized (surfaceHolder)
+                {
+                    AppHolder.getGameManager().backgroundAnimation(canvas);
+                }
+
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            finally
+            {
+                if(canvas != null)
+                {
+                    try
+                    {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                    catch (Exception e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            timeSpent = SystemClock.uptimeMillis() - kickOffTime;
+
+            if(timeSpent < WAIT)
+            {
+                try
+                {
+                    Thread.sleep(WAIT - timeSpent);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public boolean isRunning()
+    {
+        return running;
+    }
+
+    public boolean setIsRuning(boolean state)
+    {
+        running = state;
+    }
+}
