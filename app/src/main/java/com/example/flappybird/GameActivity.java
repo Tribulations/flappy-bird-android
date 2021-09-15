85caa8689677ddcbef779a998f878f57f620a68c
Tribulations <kung_jocke@hotmail.com>
2021-09-15 18:33:46 +0200

    app beautification

--- app/src/main/java/com/example/flappybird/GameActivity.java
+++ app/src/main/java/com/example/flappybird/GameActivity.java
@@ -2,6 +2,8 @@ package com.example.flappybird;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.Nullable;
 
@@ -13,6 +15,11 @@ public class GameActivity extends Activity
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         AppHolder.gameActivityContext = this;
 
         gamePlay = new GamePlay(this);
